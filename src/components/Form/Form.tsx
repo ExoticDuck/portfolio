5b046e517ced189a8a4022143b5b36f3f55680df
ExoticDuck <marchenkov.n@inbox.ru>
2022-08-30 23:07:22 +0300

    styles

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,18 +3,20 @@ import style from './Form.module.css'
 
 function Form() {
     return (<div className={style.FormBlock} id="contacts">
-        <div>
-            <h2>Contact</h2>
+        <div className={style.Card}>
+            <div className={style.Header}>
+                CONTACT ME
+            </div>
+            <div className={style.FormContainer}>
+                <form className={style.Form}>
+                    <input className={style.NameInput} placeholder="Name"></input>
+                    <input className={style.EmailInput} placeholder="Email"></input>
+                    <textarea className={style.Textarea} placeholder="Your message"></textarea>
+                    <button type='submit' className={style.Button}>Send</button>
+                </form>
+            </div>
         </div>
-        <div className={style.FormContainer}>
-            <form className={style.Form}>
-                <input className={style.NameInput} placeholder="Name"></input>
-                <input className={style.EmailInput} placeholder="Email"></input>
-                <textarea className={style.Textarea} placeholder="Your message"></textarea>
-                <button type='submit' className={style.Button}>Send</button>
-            </form>
-        </div>
-    </div> );
+    </div>);
 }
 
 export default Form;
